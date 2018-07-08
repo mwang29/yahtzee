ee5f0b61e89e4b046d3970a43221d393c9b5061c
Michael Wang <michael20995@gmail.com>
2018-07-08 19:28:43 -0400

    New score class with methods for scoring 1-6's

--- classes.py
+++ classes.py
@@ -11,3 +11,40 @@ class dice():
         for i in roll_id:
             self.values[i - 1] = r.randrange(1, 7)
         return(self.values)
+
+
+class score():
+    def __init__(self):
+        self.ones = 0
+        self.twos = 0
+        self.threes = 0
+        self.fours = 0
+        self.fives = 0
+        self.sixes = 0
+        self.three_of_a_kind = 0
+        self.four_of_a_kind = 0
+        self.full_house = 0
+        self.small_straight = 0
+        self.large_straight = 0
+        self.chance = 0
+        self.yahtzee = 0
+        self.yahtzee_bonus = 0
+        self.bonus = 0
+
+    def ones(self, player):
+        self.ones = player.count(1)
+
+    def twos(self, player):
+        self.twos = 2 * player.count(2)
+
+    def threes(self, player):
+        self.threes = 3 * player.count(3)
+
+    def fours(self, player):
+        self.fours = 4 * player.count(4)
+
+    def fives(self, player):
+        self.fives = 5 * player.count(5)
+
+    def sixes(self, player):
+        self.sixes = 5 * player.count(6)
--- game.py
+++ game.py
@@ -1,13 +1,25 @@
 from classes import dice
 
-print("Welcome to Yahtzee!")
+print("\n\nWelcome to Yahtzee!\n\n")
 start = input('Press Enter to roll your first hand!')
 
 player_1 = dice(5)  # initialize 5 dice
-print(f"Your current hand: {player_1.values}")
+print(f"\n\nYour current hand: {player_1.values}")
 
-s = input("Enter poisitions of dice you would like to re-roll. Ex: 1,2,3,\n")
-roll_id = list(map(int, s.split(",")))
+# Roll 2
+s = input("\n\nEnter poisitions of dice you would like to re-roll. Ex: 1,2,3\nIf none, type 'none'\n").lower()
+if s != "none":
+    roll_id = list(map(int, s.split(",")))
+    player_1.roll(roll_id)
+    print(f"\nYour current hand: {player_1.values}")
+else:
+    print(f"\nYour current hand: {player_1.values}")
 
-player_1.roll(roll_id)
-print(f"Your current hand: {player_1.values}")
+# Roll 3
+s = input("\n\nEnter poisitions of dice you would like to re-roll. Ex: 1,2,3\nIf none, type 'none'\n").lower()
+if s != "none":
+    roll_id = list(map(int, s.split(",")))
+    player_1.roll(roll_id)
+    print(f"\nYour current hand: {player_1.values}")
+else:
+    print(f"\nYour current hand: {player_1.values}")
